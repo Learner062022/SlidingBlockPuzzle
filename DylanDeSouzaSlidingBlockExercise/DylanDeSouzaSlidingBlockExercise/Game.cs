0ee0239fd285342fbcfd094074726a9412594608
Dylan <ddylan246@yahoo.com>
2023-10-09 22:24:18 +0800

    Add project files.

---
+++ DylanDeSouzaSlidingBlockExercise/DylanDeSouzaSlidingBlockExercise/Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Text;
+using Xamarin.Forms;
+
+namespace DylanDeSouzaSlidingBlockExercise
+{
+    public static class Game
+    {
+        public static bool won;
+
+        public static void CheckWon()
+        {
+            
+        }
+
+        public static string CheckPieceInSquare(/*rowNum, columnNum*/)
+        {
+            string piece = "";
+            return piece;
+        }
+
+        public static void SlidePiece() 
+        {
+            
+        }
+    }
+}
---
+++ DylanDeSouzaSlidingBlockExercise/DylanDeSouzaSlidingBlockExercise/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DylanDeSouzaSlidingBlockExercise
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            // RandomiseBoard
+        }
+
+        public int GetColumnEmptyBlock()
+        {
+            int columnNum = 0;
+            return columnNum;
+        }
+
+        public int GetRowEmptyBlock()
+        {
+            int RowNum = 0;
+            return RowNum;
+        }
+
+        private void OnSwipe(object sender, SwipedEventArgs e)
+        {
+            // use sender to find the column and row of the image being swiped 
+            Game.CheckWon();
+            Game.SlidePiece(/* column and row of the image being swiped */);
+        }
+
+        private void RandomiseBoard()
+        {
+
+        }
+
+        private void ResetAndRandomiseBoard(object sender, EventArgs e)
+        {
+            // RandomiseBoard
+            Game.won = false;
+        }
+    }
+}
